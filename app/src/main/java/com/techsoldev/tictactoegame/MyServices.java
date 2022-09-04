9fd3eb5514bc0e6de937d889861b74c245489099
Anas Sheikh <anassheikh@Anass-MacBook-Pro.local>
2022-09-04 09:37:47 +0500

    Initial commit

---
+++ app/src/main/java/com/techsoldev/tictactoegame/MyServices.java
@@ -0,0 +1,11 @@
+package com.techsoldev.tictactoegame;
+
+public class MyServices {
+
+    public static boolean VIBRATION_CHECK=false;
+    public static boolean SOUND_CHECK=true;
+    //this is test comment
+
+
+
+}
